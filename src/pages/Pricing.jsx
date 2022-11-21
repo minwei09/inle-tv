7bcf24fc9bab5cc502ba44f3475a59efd5f52086
minwei09 <minsitt99@gmail.com>
2022-11-21 21:16:53 +0630

    he

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -171,7 +171,7 @@ const Pricing = () => {
 
 
         <div className='flex flex-col items-center justify-center'>
-            <h1 className=' md:text-xl font-semibold text-center py-2'>{p2}</h1>
+            <h1 className='text-lg md:text-2xl font-semibold text-center py-2'>{p2}</h1>
 
             <div className='flex gap-6 justify-center py-8 text-blue'>
                 <a href="https://www.facebook.com/profile.php?id=100067427025936&mibextid=LQQJ4d" target='_blank'><BsFacebook className='w-8 h-8 cursor-pointer'/></a>
